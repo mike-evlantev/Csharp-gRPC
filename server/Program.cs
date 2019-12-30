b56ada1bb023cc9ee03c87eea661122360f4473f
Mike Evlantev <mikeev21@gmail.com>
2019-12-29 17:59:58 -0800

    Setup gRPC server

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,31 @@ namespace server
 {
     class Program
     {
+        const int Port = 50051;
         static void Main(string[] args)
         {
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("Server listening on port: " + Port);
+                Console.ReadLine();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Server failed to start: ", ex.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
         }
     }
 }
